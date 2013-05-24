93bd884455cbeee7b17c25e13ed74514b967a4cb
Gabriel Fagundez de los Reyes <gabriel.fagundez@moove-it.com>
2013-05-24 09:38:33 -0300

    Add change command

--- Comandos.hs
+++ Comandos.hs
@@ -8,7 +8,8 @@ module Comandos where
 	-- Definir Data Comando = ...
 	data Comando = 	CExitIncond | CExit | CInsertCurr | CWrite | CWriteArg Arg | CPrintCurr | Int |
 									CPrint Direc | CInsert Direc | CAppend Direc | CAppendCurr | CShow Direc | CShowCurr |
-									CPrintT Direc Direc |CShowT Direc Direc | CDelete Direc | CDeleteCurr | CDeleteT Direc Direc 
+									CPrintT Direc Direc |CShowT Direc Direc | CDelete Direc | CDeleteCurr | CDeleteT Direc Direc |
+									CChangeT Direc Direc | CChange Direc | CChangeCurr
 		deriving (Eq, Show)
 
 	data ConsoleState 	= ModoComando | ModoInsertar deriving (Eq, Ord, Show)
@@ -42,7 +43,7 @@ module Comandos where
 						`alt` comando_append_con_direccion `alt` comando_mostrar_linea_con_direccion
 						`alt` comando_imprimir_con_dos_dir `alt` comando_mostrar_linea_con_dos_dir
 						`alt` comando_borrar_linea_con_direccion `alt` comando_borrar_actual
-						`alt` comando_borrar_con_dos_dir
+						`alt` comando_borrar_con_dos_dir `alt` comando_change_con_dos_dir `alt` comando_change_con_direccion
 
 
 	-- *** *** *** *** *** *** --
@@ -76,6 +77,9 @@ module Comandos where
 	comando_show_actual :: Parse Char Comando
 	comando_show_actual = (action_parser_cond 'n') `build` const CShowCurr
 
+	comando_change_actual :: Parse Char Comando
+	comando_change_actual = (action_parser_cond 'c') `build` const CChangeCurr
+
 	
 	comando_imprimir_con_direccion :: Parse Char Comando
 	comando_imprimir_con_direccion = (directions_parser >*> (action_parser_cond 'p')) `build` \(dir, _) -> CPrint dir
@@ -92,6 +96,9 @@ module Comandos where
 	comando_borrar_linea_con_direccion :: Parse Char Comando
 	comando_borrar_linea_con_direccion = (directions_parser >*> (action_parser_cond 'd')) `build` \(dir, _) -> CDelete dir
 
+	comando_change_con_direccion :: Parse Char Comando
+	comando_change_con_direccion = (directions_parser >*> (action_parser_cond 'd')) `build` \(dir, _) -> CChange dir
+
 
 	comando_imprimir_con_dos_dir :: Parse Char Comando
 	comando_imprimir_con_dos_dir = (directions_parser >*> (token ',') >*> directions_parser >*> (action_parser_cond 'p')) `build` \(dir1,(_,(dir2, _))) -> CPrintT dir1 dir2
@@ -102,6 +109,8 @@ module Comandos where
 	comando_borrar_con_dos_dir :: Parse Char Comando
 	comando_borrar_con_dos_dir = (directions_parser >*> (token ',') >*> directions_parser >*> (action_parser_cond 'd')) `build` \(dir1,(_,(dir2, _))) -> CDeleteT dir1 dir2
 
+	comando_change_con_dos_dir :: Parse Char Comando
+	comando_change_con_dos_dir = (directions_parser >*> (token ',') >*> directions_parser >*> (action_parser_cond 'c')) `build` \(dir1,(_,(dir2, _))) -> CChangeT dir1 dir2
 
 	-- *** *** *** *** *** *** --
 	-- Ejecucion de los comandos
@@ -135,15 +144,18 @@ module Comandos where
 		| (comando == Just CAppendCurr)													= ejecutar_comando_append_current comando st
 		| (comando == Just CDeleteCurr)													=	ejecutar_comando_delete_current comando st
 		| (comando == Just CShowCurr)														= ejecutar_comando_show_current comando st
+		| (comando == Just CChangeCurr)													= ejecutar_comando_change_current comando st
 	ejecutar_comando_modo_comando (Just (CWriteArg arg)) st 	= ejecutar_comando_write_con_ruta (Just (CWriteArg arg)) st
 	ejecutar_comando_modo_comando (Just (CPrint direc)) st 		= ejecutar_comando_print_con_dir (Just (CPrint direc)) st
 	ejecutar_comando_modo_comando (Just (CInsert direc)) st 	= ejecutar_comando_insert_con_dir (Just (CInsert direc)) st
 	ejecutar_comando_modo_comando (Just (CAppend direc)) st 	= ejecutar_comando_append_con_dir (Just (CAppend direc)) st
 	ejecutar_comando_modo_comando (Just (CShow direc)) st 		= ejecutar_comando_show_con_dir (Just (CShow direc)) st
 	ejecutar_comando_modo_comando (Just (CDelete direc)) st 	= ejecutar_comando_delete_con_dir (Just (CDelete direc)) st  
+	ejecutar_comando_modo_comando (Just (CChange direc)) st 	= ejecutar_comando_change_con_dir (Just (CChange direc)) st  
 	ejecutar_comando_modo_comando (Just (CPrintT direc1 direc2)) st 		= ejecutar_comando_print_con_dos_dir (Just (CPrintT direc1 direc2)) st
  	ejecutar_comando_modo_comando (Just (CShowT direc1 direc2)) st 			= ejecutar_comando_show_con_dos_dir (Just (CShowT direc1 direc2)) st
- 	ejecutar_comando_modo_comando (Just (CDeleteT direc1 direc2)) st 		= ejecutar_comando_delete_con_dos_dir (Just (CShowT direc1 direc2)) st
+ 	ejecutar_comando_modo_comando (Just (CDeleteT direc1 direc2)) st 		= ejecutar_comando_delete_con_dos_dir (Just (CDeleteT direc1 direc2)) st
+ 	ejecutar_comando_modo_comando (Just (CChangeT direc1 direc2)) st 		= ejecutar_comando_change_con_dos_dir (Just (CChangeT direc1 direc2)) st
 
 
 
@@ -192,6 +204,11 @@ module Comandos where
 
 	ejecutar_comando_delete_current :: Maybe Comando -> State -> (String, State)
 	ejecutar_comando_delete_current comando st = borrar_linea linea st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+
+	ejecutar_comando_change_current :: Maybe Comando -> State -> (String, State)
+	ejecutar_comando_change_current comando st = borrar_linea_change linea st
 		where 
 			(linea, buf, modo, esta_modificado, _, nom_arch) = st
 
@@ -359,25 +376,25 @@ module Comandos where
 			offset = foldr (+) 0 off 
 
 	ejecutar_comando_delete_con_dir :: Maybe Comando -> State -> (String, State)
-	ejecutar_comando_delete_con_dir (Just (CShow (Direc Ultima off))) st =
+	ejecutar_comando_delete_con_dir (Just (CDelete (Direc Ultima off))) st =
 		borrar_linea (maximo + offset) st
 		where 
 			(linea, buf, modo, esta_modificado, _, nom_arch) = st
 			maximo = length buf
 			offset = foldr (+) 0 off
-	ejecutar_comando_delete_con_dir (Just (CShow (Direc Corriente off))) st = 
+	ejecutar_comando_delete_con_dir (Just (CDelete (Direc Corriente off))) st = 
 		borrar_linea (linea + offset) st
 		where 
 			(linea, buf, modo, esta_modificado, _, nom_arch) = st
 			maximo = length buf
 			offset = foldr (+) 0 off
-	ejecutar_comando_delete_con_dir (Just (CShow (Direc (Abs a) off))) st =
+	ejecutar_comando_delete_con_dir (Just (CDelete (Direc (Abs a) off))) st =
 		borrar_linea (a + offset) st
 		where 
 			(linea, buf, modo, esta_modificado, _, nom_arch) = st
 			maximo = length buf
 			offset = foldr (+) 0 off
-	ejecutar_comando_delete_con_dir (Just (CShow (Direc (Rel a) off))) st =
+	ejecutar_comando_delete_con_dir (Just (CDelete (Direc (Rel a) off))) st =
 		borrar_linea (a + offset) st
 		where 
 			(linea, buf, modo, esta_modificado, _, nom_arch) = st
@@ -395,8 +412,42 @@ module Comandos where
 	borrar_linea_buf linea [] = []
 	borrar_linea_buf linea arr = take (linea-1) arr ++ drop linea arr
 
+	ejecutar_comando_change_con_dir :: Maybe Comando -> State -> (String, State)
+	ejecutar_comando_change_con_dir (Just (CChange (Direc Ultima off))) st =
+		borrar_linea_change (maximo + offset) st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+			offset = foldr (+) 0 off
+	ejecutar_comando_change_con_dir (Just (CChange (Direc Corriente off))) st = 
+		borrar_linea_change (linea + offset) st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+			offset = foldr (+) 0 off
+	ejecutar_comando_change_con_dir (Just (CChange (Direc (Abs a) off))) st =
+		borrar_linea_change (a + offset) st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+			offset = foldr (+) 0 off
+	ejecutar_comando_change_con_dir (Just (CChange (Direc (Rel a) off))) st =
+		borrar_linea_change (a + offset) st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+			offset = foldr (+) 0 off
 
+	borrar_linea_change a st 
+		| a <= 0 				= ("?\n", (linea, buf, modo, esta_modificado, 'I', nom_arch))
+		| a > maximo 		= ("?\n", (linea, buf, modo, esta_modificado, 'I', nom_arch))
+		| otherwise			= ("", (a, borrar_linea_change_buf linea buf, ModoInsertar, esta_modificado, 'c', nom_arch))
+		where  	
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
 
+	borrar_linea_change_buf linea [] = []
+	borrar_linea_change_buf linea arr = take (linea-1) arr ++ drop linea arr
 
 	-- *** *** *** *** *** *** --
 	-- Ejecucion de comandos con dos direcciones
@@ -771,15 +822,142 @@ module Comandos where
 
 	ejecutar_comando_delete_automatico :: Int -> Int -> State -> (String, State)
 	ejecutar_comando_delete_automatico indice1 indice2 st
-		| indice1 > maximo				= ("?\n", (linea, buf, modo, esta_modificado, 'n', nom_arch))	
-		| indice2 > maximo				= ("?\n", (linea, buf, modo, esta_modificado, 'n', nom_arch))	
-		| indice1 <= 0						= ("?\n", (linea, buf, modo, esta_modificado, 'n', nom_arch))	
-		|	indice1 <= 0						= ("?\n", (linea, buf, modo, esta_modificado, 'n', nom_arch))	
-		| indice1 > indice2 			= ("?\n", (linea, buf, modo, esta_modificado, 'n', nom_arch))	
+		| indice1 > maximo				= ("?\n", (linea, buf, modo, esta_modificado, 'd', nom_arch))	
+		| indice2 > maximo				= ("?\n", (linea, buf, modo, esta_modificado, 'd', nom_arch))	
+		| indice1 <= 0						= ("?\n", (linea, buf, modo, esta_modificado, 'd', nom_arch))	
+		|	indice1 <= 0						= ("?\n", (linea, buf, modo, esta_modificado, 'd', nom_arch))	
+		| indice1 > indice2 			= ("?\n", (linea, buf, modo, esta_modificado, 'd', nom_arch))	
 		| otherwise 							= ("", (indice1, borrar_lineas indice1 indice2 buf, modo, True, 'I', nom_arch))
 		where 
 			(linea, buf, modo, esta_modificado, _, nom_arch) = st
 			maximo = length buf
+
+	ejecutar_comando_change_con_dos_dir :: Maybe Comando -> State -> (String, State)
+	ejecutar_comando_change_con_dos_dir (Just (CChangeT (Direc Ultima off1) (Direc Ultima off2))) st = 
+		ejecutar_comando_change_automatico (maximo + offset1) (maximo + offset2) st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+			offset1 = foldr (+) 0 off1
+			offset2 = foldr (+) 0 off2
+	ejecutar_comando_change_con_dos_dir (Just (CChangeT (Direc Corriente off1) (Direc Corriente off2))) st = 
+		ejecutar_comando_change_automatico (linea + offset1) (linea + offset2) st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+			offset1 = foldr (+) 0 off1
+			offset2 = foldr (+) 0 off2
+	ejecutar_comando_change_con_dos_dir (Just (CChangeT (Direc (Abs a) off1) (Direc (Abs b) off2))) st = 
+		ejecutar_comando_change_automatico (a + offset1) (b + offset2) st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+			offset1 = foldr (+) 0 off1
+			offset2 = foldr (+) 0 off2
+	ejecutar_comando_change_con_dos_dir (Just (CChangeT (Direc (Rel a) off1) (Direc (Rel b) off2))) st = 
+		ejecutar_comando_change_automatico (a + offset1) (b + offset2) st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+			offset1 = foldr (+) 0 off1
+			offset2 = foldr (+) 0 off2
+	ejecutar_comando_change_con_dos_dir (Just (CChangeT (Direc Corriente off1) (Direc Ultima off2))) st = 
+		ejecutar_comando_change_automatico (linea + offset1) (maximo + offset2) st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+			offset1 = foldr (+) 0 off1
+			offset2 = foldr (+) 0 off2
+	ejecutar_comando_change_con_dos_dir (Just (CChangeT (Direc Ultima off1) (Direc Corriente off2))) st = 
+		ejecutar_comando_change_automatico (maximo + offset1) (linea + offset2) st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+			offset1 = foldr (+) 0 off1
+			offset2 = foldr (+) 0 off2
+	ejecutar_comando_change_con_dos_dir (Just (CChangeT (Direc (Abs a) off1) (Direc (Rel b) off2))) st = 
+		ejecutar_comando_change_automatico (a + offset1) (b + offset2) st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+			offset1 = foldr (+) 0 off1
+			offset2 = foldr (+) 0 off2
+	ejecutar_comando_change_con_dos_dir (Just (CChangeT (Direc (Rel a) off1) (Direc (Abs b) off2))) st = 
+		ejecutar_comando_change_automatico (a + offset1) (b + offset2) st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+			offset1 = foldr (+) 0 off1
+			offset2 = foldr (+) 0 off2
+	ejecutar_comando_change_con_dos_dir (Just (CChangeT (Direc Ultima off1) (Direc (Rel b) off2))) st = 
+		ejecutar_comando_change_automatico (maximo + offset1) (b + offset2) st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+			offset1 = foldr (+) 0 off1
+			offset2 = foldr (+) 0 off2
+	ejecutar_comando_change_con_dos_dir (Just (CChangeT (Direc (Rel a) off1) (Direc Ultima off2))) st = 
+		ejecutar_comando_change_automatico (a + offset1) (maximo + offset2) st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+			offset1 = foldr (+) 0 off1
+			offset2 = foldr (+) 0 off2
+	ejecutar_comando_change_con_dos_dir (Just (CChangeT (Direc Ultima off1) (Direc (Abs b) off2))) st = 
+		ejecutar_comando_change_automatico (maximo + offset1) (b + offset2) st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+			offset1 = foldr (+) 0 off1
+			offset2 = foldr (+) 0 off2
+	ejecutar_comando_change_con_dos_dir (Just (CChangeT (Direc (Abs a) off1) (Direc Ultima off2))) st = 
+		ejecutar_comando_change_automatico (a + offset1) (maximo + offset2) st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+			offset1 = foldr (+) 0 off1
+			offset2 = foldr (+) 0 off2
+	ejecutar_comando_change_con_dos_dir (Just (CChangeT (Direc Corriente off1) (Direc (Rel b) off2))) st = 
+		ejecutar_comando_change_automatico (linea + offset1) (b + offset2) st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+			offset1 = foldr (+) 0 off1
+			offset2 = foldr (+) 0 off2
+	ejecutar_comando_change_con_dos_dir (Just (CChangeT (Direc (Rel a) off1) (Direc Corriente off2))) st = 
+		ejecutar_comando_change_automatico (a + offset1) (linea + offset2) st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+			offset1 = foldr (+) 0 off1
+			offset2 = foldr (+) 0 off2
+	ejecutar_comando_change_con_dos_dir (Just (CChangeT (Direc Corriente off1) (Direc (Abs b) off2))) st = 
+		ejecutar_comando_change_automatico (linea + offset1) (b + offset2) st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+			offset1 = foldr (+) 0 off1
+			offset2 = foldr (+) 0 off2
+	ejecutar_comando_change_con_dos_dir (Just (CChangeT (Direc (Abs a) off1) (Direc Corriente off2))) st = 
+		ejecutar_comando_change_automatico (a + offset1) (linea + offset2) st
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+			offset1 = foldr (+) 0 off1
+			offset2 = foldr (+) 0 off2
+
+	ejecutar_comando_change_automatico :: Int -> Int -> State -> (String, State)
+	ejecutar_comando_change_automatico indice1 indice2 st
+		| indice1 > maximo				= ("?\n", (linea, buf, modo, esta_modificado, 'c', nom_arch))	
+		| indice2 > maximo				= ("?\n", (linea, buf, modo, esta_modificado, 'c', nom_arch))	
+		| indice1 <= 0						= ("?\n", (linea, buf, modo, esta_modificado, 'c', nom_arch))	
+		|	indice1 <= 0						= ("?\n", (linea, buf, modo, esta_modificado, 'c', nom_arch))	
+		| indice1 > indice2 			= ("?\n", (linea, buf, modo, esta_modificado, 'c', nom_arch))	
+		| otherwise 							= ("", (indice1, borrar_lineas indice1 indice2 buf, ModoInsertar, True, 'c', nom_arch))
+		where 
+			(linea, buf, modo, esta_modificado, _, nom_arch) = st
+			maximo = length buf
+
 
 	borrar_lineas :: Int -> Int -> [a] -> [a]
 	borrar_lineas indice1 indice2 buf = take (indice1-1) buf ++ drop indice2 buf
